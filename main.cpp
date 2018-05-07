969b2541c4edefe6806ff5222ff71f7f1a9c71d6
MadCreeper <33979935+MadCreeper@users.noreply.github.com>
2018-05-07 23:00:00 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+void read_file()
+{
+    FILE *image1, *file1,*targetImage;
+    char buf[100];
+
+    printf("Input Original Image:\n");
+    scanf("%s",buf);
+    printf("Input Chosen File:\n");
+    scanf("%s",buf);
+    printf("Input  Image:\n");
+    scanf("%s",buf);
+
+}
+
+int main(){
+
+
+
+}
